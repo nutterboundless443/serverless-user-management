aae657efe5d3aade309f1e6e89421aada9a6c30e
nutterboundless443 <serhiisiraj@hotmail.com>
2025-09-02 11:43:06 -0500

    Fix logging for user registration

--- handler.js
+++ handler.js
@@ -22,6 +22,7 @@ module.exports.register = async (event) => {
         }
     };
     await dynamoDB.put(params).promise();
+    console.log(`User registered: ${data.userId}`); // Added logging for user registration
     return { statusCode: 200, body: JSON.stringify({ message: 'User registered successfully' }) };
 };
 
